8b10fd9e655da10d145cd2c34d9aa6da16d3f5b4
luk-f-a <lucioarjona@hotmail.com>
2017-11-04 12:32:52 +0100

    added first draft geometric bm

--- esglil/equity_models.py
+++ esglil/equity_models.py
@@ -6,3 +6,32 @@ Created on Tue Oct 24 20:28:07 2017
 @author: lucio
 """
 
+class GeometricBrownianMotion(object):
+    """class for Geometric Brownian Motion model of equity returns
+
+     Parameters
+    ----------
+    """
+    __slots__ = ('mu', 'sigma')
+    
+    def __init__(self, mu=None, sigma=None):
+        self.mu = mu
+        self.sigma = sigma
+        
+    def _check_valid_params():
+        pass
+        
+    def generate(self, X):
+        """produces simulations of  Geometric Brownian Motion
+        
+        Parameters
+        ----------       
+        X array of Brownian motion simulations
+        """
+        assert not (self.mu is None or self.sigma is None)
+        self._check_valid_params()
+        return np.exp(mu)
+        
+        
+        
+    
