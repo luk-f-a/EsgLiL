ee52d756c8d51999b1c497dfe7c666569589f994
luk-f-a <lucioarjona@hotmail.com>
2017-11-28 17:58:02 +0000

    started leakage tests

--- tests/test_hw1f.py
+++ tests/test_hw1f.py
@@ -20,161 +20,230 @@ from scipy.stats import normaltest, kstest
 #TODO: test delta_t_in and out different than one and different than each other
 
         
-class hw1f_test_short_rate_xr(unittest.TestCase):
-    def setUp(self):
-        self.rng = rng.NormalRng(shape={'svar':1, 'sim':10, 'timestep':5},
-                                mean=[0], cov=[[1]])
-        HW = ir_models.HullWhite1fModel
-        fixed_b = 0.05
-        b = lambda x: fixed_b
-        self.r_zero = 0.05
-        self.hw = HW(a=1.01, b=b, sigma=0.02, r_zero=self.r_zero, 
-                      delta_t_in=1, delta_t_out=1)
-        
-    def test_shape(self):
-        X = self.rng.generate()
-        sims = self.hw.transform(X)
-        self.assertEqual(type(sims), xr.DataArray,
-                         'incorrect type')
-        self.assertEqual(sims.shape, (1, 10,5))
+#class hw1f_test_short_rate_xr(unittest.TestCase):
+#    def setUp(self):
+#        self.rng = rng.NormalRng(shape={'svar':1, 'sim':10, 'timestep':5},
+#                                mean=[0], cov=[[1]])
+#        HW = ir_models.HullWhite1fModel
+#        fixed_b = 0.05
+#        b = lambda x: fixed_b
+#        self.r_zero = 0.05
+#        self.hw = HW(a=1.01, b=b, sigma=0.02, r_zero=self.r_zero, 
+#                      delta_t_in=1, delta_t_out=1)
+#        
+#    def test_shape(self):
+#        X = self.rng.generate()
+#        sims = self.hw.transform(X)
+#        self.assertEqual(type(sims), xr.DataArray,
+#                         'incorrect type')
+#        self.assertEqual(sims.shape, (1, 10,5))
 #        for r in sims.values.squeeze():
 #            row =  np.insert(r,0,self.r_zero)
 #            x = np.insert(sims.coords['timestep'].values,0,0)
 #            plt.plot(x, row)
 
-class hw1f_stat_test_short_rate(unittest.TestCase):            
+class hw1f_leakage_tests(unittest.TestCase):    
     def setUp(self):
-        self.time_sampling_factor = k = 10
+        self.time_sampling_factor = k = 100
         self.max_time = T = 10
-        self.rng = rng.NormalRng(shape={'svar':1, 'sim':100_000, 
+        self.rng = rng.NormalRng(shape={'svar':1, 'sim':1000, 
                                         'timestep':T*k},
                                 mean=[0], cov=[[1/k]])
         self.X = self.rng.generate()
-    def test_fixed_mean_reversion1(self):
-        """Test that starting from a level it stays in that level
+        
+        bond_prices = {1: 1.0073, 2: 1.0111, 3: 1.0136, 4: 1.0138, 5: 1.0115, 6: 1.0072,
+              7: 1.0013, 8: 0.9941, 9: 0.9852, 10: 0.9751, 11: 0.9669, 12: 0.9562,
+              13: 0.9461, 14: 0.937, 15: 0.9281, 16: 0.9188, 17: 0.9091, 18: 0.8992, 
+              19: 0.8894, 20: 0.8797, 21: 0.8704, 22: 0.8614, 23: 0.8527, 24: 0.8442, 
+              25: 0.836, 26: 0.828, 27: 0.8202, 28: 0.8127, 29: 0.8053, 30: 0.7983, 
+              31: 0.7914, 32: 0.7848, 33: 0.7783, 34: 0.772, 35: 0.7659, 36: 0.7599, 
+              37: 0.7539, 38: 0.7481, 39: 0.7423, 40: 0.7366 }
+        swaption_prices = [
+                 {'normal_vol': 0.004216, 'start': 1, 'strike': 'ATM', 'tenor': 1},
+                 {'normal_vol': 0.00519, 'start': 2, 'strike': 'ATM', 'tenor': 1},
+                 {'normal_vol': 0.006195, 'start': 3, 'strike': 'ATM', 'tenor': 1},
+                 {'normal_vol': 0.007097, 'start': 5, 'strike': 'ATM', 'tenor': 1},
+                 {'normal_vol': 0.007277, 'start': 7, 'strike': 'ATM', 'tenor': 1},
+                 {'normal_vol': 0.007368, 'start': 10, 'strike': 'ATM', 'tenor': 1},
+                 {'normal_vol': 0.004332, 'start': 1, 'strike': 'ATM', 'tenor': 2},
+                 {'normal_vol': 0.00532, 'start': 2, 'strike': 'ATM', 'tenor': 2},
+                 {'normal_vol': 0.0063, 'start': 3, 'strike': 'ATM', 'tenor': 2},
+                 {'normal_vol': 0.00729, 'start': 5, 'strike': 'ATM', 'tenor': 2},
+                 {'normal_vol': 0.007424, 'start': 7, 'strike': 'ATM', 'tenor': 2},
+                 {'normal_vol': 0.00749, 'start': 10, 'strike': 'ATM', 'tenor': 2},
+                 {'normal_vol': 0.004378, 'start': 1, 'strike': 'ATM', 'tenor': 3},
+                 {'normal_vol': 0.005424, 'start': 2, 'strike': 'ATM', 'tenor': 3},
+                 {'normal_vol': 0.006434, 'start': 3, 'strike': 'ATM', 'tenor': 3},
+                 {'normal_vol': 0.007391, 'start': 5, 'strike': 'ATM', 'tenor': 3},
+                 {'normal_vol': 0.007406, 'start': 7, 'strike': 'ATM', 'tenor': 3},
+                 {'normal_vol': 0.007439, 'start': 10, 'strike': 'ATM', 'tenor': 3},
+                 {'normal_vol': 0.004571, 'start': 1, 'strike': 'ATM', 'tenor': 5},
+                 {'normal_vol': 0.005691, 'start': 2, 'strike': 'ATM', 'tenor': 5},
+                 {'normal_vol': 0.006551, 'start': 3, 'strike': 'ATM', 'tenor': 5},
+                 {'normal_vol': 0.007506, 'start': 5, 'strike': 'ATM', 'tenor': 5},
+                 {'normal_vol': 0.007473, 'start': 7, 'strike': 'ATM', 'tenor': 5},
+                 {'normal_vol': 0.007376, 'start': 10, 'strike': 'ATM', 'tenor': 5},
+                 {'normal_vol': 0.004941, 'start': 1, 'strike': 'ATM', 'tenor': 7},
+                 {'normal_vol': 0.00593, 'start': 2, 'strike': 'ATM', 'tenor': 7},
+                 {'normal_vol': 0.006794, 'start': 3, 'strike': 'ATM', 'tenor': 7},
+                 {'normal_vol': 0.0076, 'start': 5, 'strike': 'ATM', 'tenor': 7},
+                 {'normal_vol': 0.007646, 'start': 7, 'strike': 'ATM', 'tenor': 7},
+                 {'normal_vol': 0.007412, 'start': 10, 'strike': 'ATM', 'tenor': 7}]
+        a = 0.01
+        sigma = ir_models.hw1f_sigma_calibration(bond_prices, 
+                                                    swaption_prices, a)
+
+        B, r_0 =  ir_models.hw1f_B_function(bond_prices, a, sigma)
+        HW = ir_models.HullWhite1fModel
+
+        
+
+        hw = HW(a=a, B=B, sigma=sigma, r_zero=r_0, 
+                      delta_t_in=1/self.time_sampling_factor, delta_t_out=1,
+                      bond_prices=bond_prices)
+        self.sims = hw.transform(self.X)    
+        
+    def test_bond_prices(self):
+        """Test that starting bond prices can be recovered
         """
-       
-        HW = ir_models.HullWhite1fModel
-        a = 0.5
-        mean_rev_level = 0.05
-        fixed_b = mean_rev_level*a
-        b = lambda x: fixed_b
-        r_zero = 0.05
-        hw = HW(a=a, b=b, sigma=0.02, r_zero=r_zero, 
-                      delta_t_in=1/self.time_sampling_factor, delta_t_out=1)
-
-        sims = hw.transform(self.X)
-        mean_ = sims.mean(dim='sim').values.squeeze()
-        self.assertTrue(np.allclose(mean_, [mean_rev_level]*10, rtol=0.01))
-
-    def test_fixed_mean_reversion2(self):
-        """Test that starting from a high level it reverts
-        """
-        HW = ir_models.HullWhite1fModel
-        a = 0.5
-        mean_rev_level = 0.05
-        fixed_b = mean_rev_level*a
-        b = lambda x: fixed_b
-        r_zero = 0.30
-        hw = HW(a=a, b=b, sigma=0.02, r_zero=r_zero, 
-                      delta_t_in=1/self.time_sampling_factor, delta_t_out=1)
-
-        sims = hw.transform(self.X)
-        mean_ = sims.mean(dim='sim').values.squeeze()
-        dist = (mean_- np.array([mean_rev_level]*10))
-        diff_dist = np.diff(dist)
-        self.assertTrue(np.all(diff_dist<0))
-        
-    def test_distribution(self):
-        """Test that when starting from a level it stays in that level
-        """
-        k = 5_000
-        T = 1
-        sim_nb = 1000
-        self.rng = rng.NormalRng(shape={'svar':1, 'sim':sim_nb, 
-                                        'timestep':T*k},
-                                mean=[0], cov=[[1/k]])
-        self.X = self.rng.generate()
-        res = True
-        HW = ir_models.HullWhite1fModel
-        a = 0.5
-        mean_rev_level = 0.05
-        fixed_b = mean_rev_level*a
-        b = lambda x: fixed_b
-        r_zero = 0.30
-        sigma = 0.02
-        hw = HW(a=a, b=b, sigma=sigma, r_zero=r_zero, 
-                      delta_t_in=1/k, delta_t_out=1)
-        test_time = 1
-        test_ts = test_time*k
-        sims = hw.transform(self.X)
-        sims_test = sims.loc[{'timestep': test_ts}]
-        theoretical_mu = np.exp(-a*test_time)*r_zero+b(test_time)/a*(1-np.exp(-a*test_time))
-        theoretical_sigma = np.sqrt(sigma**2/2/a*(1-np.exp(-2*a*test_time)))
- 
-        if sim_nb > 1000:
-            _, p1 = normaltest(sims_test.values.squeeze())
-        else:
-            _, p1 = kstest(sims_test.values.squeeze(), 'norm',
-                           args=(theoretical_mu,theoretical_sigma) )
-        if p1 < 0.1:
-            print('Normality Test failed: p-value {}'.format(p1))
-            res = False
+        import pandas as pd
+        self.sims.to_dataframe(name='test').unstack(['svar', 'timestep']).to_clipboard()
         
         
-        if round(float(sims_test.mean().squeeze()), 3)!= round(theoretical_mu,3):   
-            a = float(sims_test.mean().squeeze())
-            b = theoretical_mu
-            print('Fail:_Empirical mean {}, closed form mean {}'.format(a, b))            
-            res = False
-        if round(float(sims_test.std().squeeze()), 3)!= round(theoretical_sigma,3):   
-            a = float(sims_test.std().squeeze())
-            b = theoretical_sigma
-            print('Fail: Empirical std dev {}, closed form std dev {}'.format(a, b))            
-            res = False            
-       
-        self.assertTrue(res)
         
-class hw1f_test_short_rate_and_bonds_xr(unittest.TestCase):
-    def setUp(self):
-        self.rng = rng.NormalRng(shape={'svar':1, 'sim':10, 'timestep':5},
-                                mean=[0], cov=[[1]])
-        HW = ir_models.HullWhite1fModel
-        fixed_b = 0.05
-        b = lambda x: fixed_b
-        self.r_zero = 0.05
-        bond_p = {1: 99, 2:98}
-        self.hw = HW(a=1.01, b=b, sigma=0.02, r_zero=self.r_zero, 
-                      delta_t_in=1, delta_t_out=1,  bond_prices=bond_p)
-        
-    def test_shape(self):
-        X = self.rng.generate()
-        sims = self.hw.transform(X)
-        self.assertEqual(type(sims), xr.DataArray,
-                         'incorrect type')
-        self.assertEqual(sims.shape, (3, 10,5))
-
-
-class pipeline_hw1f_test_xr(unittest.TestCase):
-    """Test pipeline uniform rng with numpy output
-    """
-    def setUp(self):
-        self.rng = rng.NormalRng(shape={'svar':1, 'sim':10, 'timestep':5},
-                                mean=[0], cov=[[1]])
-        HW = ir_models.HullWhite1fModel
-        fixed_b = 0.05
-        b = lambda x: fixed_b
-        self.r_zero = 0.05
-        self.hw = HW(a=1.01, b=b, sigma=0.02, r_zero=self.r_zero, 
-                      delta_t_in=1, delta_t_out=1)
- 
-        self.ppl = pipeline.Pipeline([self.rng, self.hw])
-        
-    def test_returns_object(self):
-        r_nb = self.ppl.generate()
-        self.assertEqual(type(r_nb), xr.DataArray,
-                         'incorrect type')
-        self.assertEqual(r_nb.shape, (1, 10,5))
+#class hw1f_stat_test_short_rate(unittest.TestCase):            
+#    def setUp(self):
+#        self.time_sampling_factor = k = 10
+#        self.max_time = T = 10
+#        self.rng = rng.NormalRng(shape={'svar':1, 'sim':100_000, 
+#                                        'timestep':T*k},
+#                                mean=[0], cov=[[1/k]])
+#        self.X = self.rng.generate()
+#    def test_fixed_mean_reversion1(self):
+#        """Test that starting from a level it stays in that level
+#        """
+#       
+#        HW = ir_models.HullWhite1fModel
+#        a = 0.5
+#        mean_rev_level = 0.05
+#        fixed_b = mean_rev_level*a
+#        b = lambda x: fixed_b
+#        r_zero = 0.05
+#        hw = HW(a=a, b=b, sigma=0.02, r_zero=r_zero, 
+#                      delta_t_in=1/self.time_sampling_factor, delta_t_out=1)
+#
+#        sims = hw.transform(self.X)
+#        mean_ = sims.mean(dim='sim').values.squeeze()
+#        self.assertTrue(np.allclose(mean_, [mean_rev_level]*10, rtol=0.01))
+#
+#    def test_fixed_mean_reversion2(self):
+#        """Test that starting from a high level it reverts
+#        """
+#        HW = ir_models.HullWhite1fModel
+#        a = 0.5
+#        mean_rev_level = 0.05
+#        fixed_b = mean_rev_level*a
+#        b = lambda x: fixed_b
+#        r_zero = 0.30
+#        hw = HW(a=a, b=b, sigma=0.02, r_zero=r_zero, 
+#                      delta_t_in=1/self.time_sampling_factor, delta_t_out=1)
+#
+#        sims = hw.transform(self.X)
+#        mean_ = sims.mean(dim='sim').values.squeeze()
+#        dist = (mean_- np.array([mean_rev_level]*10))
+#        diff_dist = np.diff(dist)
+#        self.assertTrue(np.all(diff_dist<0))
+#        
+#    def test_distribution(self):
+#        """Test that when starting from a level it stays in that level
+#        """
+#        k = 5_000
+#        T = 1
+#        sim_nb = 1000
+#        self.rng = rng.NormalRng(shape={'svar':1, 'sim':sim_nb, 
+#                                        'timestep':T*k},
+#                                mean=[0], cov=[[1/k]])
+#        self.X = self.rng.generate()
+#        res = True
+#        HW = ir_models.HullWhite1fModel
+#        a = 0.5
+#        mean_rev_level = 0.05
+#        fixed_b = mean_rev_level*a
+#        b = lambda x: fixed_b
+#        r_zero = 0.30
+#        sigma = 0.02
+#        hw = HW(a=a, b=b, sigma=sigma, r_zero=r_zero, 
+#                      delta_t_in=1/k, delta_t_out=1)
+#        test_time = 1
+#        test_ts = test_time*k
+#        sims = hw.transform(self.X)
+#        sims_test = sims.loc[{'timestep': test_ts}]
+#        theoretical_mu = np.exp(-a*test_time)*r_zero+b(test_time)/a*(1-np.exp(-a*test_time))
+#        theoretical_sigma = np.sqrt(sigma**2/2/a*(1-np.exp(-2*a*test_time)))
+# 
+#        if sim_nb > 1000:
+#            _, p1 = normaltest(sims_test.values.squeeze())
+#        else:
+#            _, p1 = kstest(sims_test.values.squeeze(), 'norm',
+#                           args=(theoretical_mu,theoretical_sigma) )
+#        if p1 < 0.1:
+#            print('Normality Test failed: p-value {}'.format(p1))
+#            res = False
+#        
+#        
+#        if round(float(sims_test.mean().squeeze()), 3)!= round(theoretical_mu,3):   
+#            a = float(sims_test.mean().squeeze())
+#            b = theoretical_mu
+#            print('Fail:_Empirical mean {}, closed form mean {}'.format(a, b))            
+#            res = False
+#        if round(float(sims_test.std().squeeze()), 3)!= round(theoretical_sigma,3):   
+#            a = float(sims_test.std().squeeze())
+#            b = theoretical_sigma
+#            print('Fail: Empirical std dev {}, closed form std dev {}'.format(a, b))            
+#            res = False            
+#       
+#        self.assertTrue(res)
+#        
+#class hw1f_test_short_rate_and_bonds_xr(unittest.TestCase):
+#    def setUp(self):
+#        self.rng = rng.NormalRng(shape={'svar':1, 'sim':10, 'timestep':5},
+#                                mean=[0], cov=[[1]])
+#        HW = ir_models.HullWhite1fModel
+#        fixed_b = 0.05
+#        b = lambda x: fixed_b
+#        self.r_zero = 0.05
+#        bond_p = {1: 99, 2:98}
+#        self.hw = HW(a=1.01, b=b, sigma=0.02, r_zero=self.r_zero, 
+#                      delta_t_in=1, delta_t_out=1,  bond_prices=bond_p)
+#        
+#    def test_shape(self):
+#        X = self.rng.generate()
+#        sims = self.hw.transform(X)
+#        self.assertEqual(type(sims), xr.DataArray,
+#                         'incorrect type')
+#        self.assertEqual(sims.shape, (3, 10,5))
+#
+#
+#class pipeline_hw1f_test_xr(unittest.TestCase):
+#    """Test pipeline uniform rng with numpy output
+#    """
+#    def setUp(self):
+#        self.rng = rng.NormalRng(shape={'svar':1, 'sim':10, 'timestep':5},
+#                                mean=[0], cov=[[1]])
+#        HW = ir_models.HullWhite1fModel
+#        fixed_b = 0.05
+#        b = lambda x: fixed_b
+#        self.r_zero = 0.05
+#        self.hw = HW(a=1.01, b=b, sigma=0.02, r_zero=self.r_zero, 
+#                      delta_t_in=1, delta_t_out=1)
+# 
+#        self.ppl = pipeline.Pipeline([self.rng, self.hw])
+#        
+#    def test_returns_object(self):
+#        r_nb = self.ppl.generate()
+#        self.assertEqual(type(r_nb), xr.DataArray,
+#                         'incorrect type')
+#        self.assertEqual(r_nb.shape, (1, 10,5))
 
     
         
