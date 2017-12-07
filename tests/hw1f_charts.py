906b61cbce8f9ff8940b31a7862c4d9ef708a564
luk-f-a <lucioarjona@hotmail.com>
2017-12-07 18:23:20 +0000

    Update hw1f_charts.py

--- tests/hw1f_charts.py
+++ tests/hw1f_charts.py
@@ -9,57 +9,63 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.getcwd()))
 from esglil import rng
-from esglil import pipeline
+from esglil.esg import ESG
+from esglil.common import TimeDependentParameter
+from esglil.ir_models import HullWhite1fShortRate, HullWhite1fBondPrice, HullWhite1fCashAccount
 from esglil import ir_models
+
 import numpy as np
 import xarray as xr
 from matplotlib import pyplot as plt
 import datetime
 from scipy.stats import normaltest, shapiro, probplot, kstest
 import matplotlib.mlab as mlab
+import pandas as pd
 
 def plot_short_rate_all_sims():
-    now_ = datetime.datetime.now()
-    r_gen = rng.NormalRng(shape={'svar':1, 'sim':10, 'timestep':5},
-                            mean=[0], cov=[[1]])
-    HW = ir_models.HullWhite1fModel
-    fixed_b = 0.05
-    b = lambda x: fixed_b
-    r_zero = 0.05
-    hw = HW(a=1.01, b=b, sigma=0.02, r_zero=r_zero, 
-                  delta_t_in=1, delta_t_out=1)
-    
-    
-    X = r_gen.generate()
-    sims = hw.transform(X)
+    delta_t = 1
+    n_sims= 10
+    dW = rng.NormalRng(dims=1, sims=n_sims, mean=[0], cov=[[delta_t]])
+    a = 0.001
+    sigma = 0.01
+    B = TimeDependentParameter(function=lambda t: 0.01)
+    r = HullWhite1fShortRate(B=B, a=a, sigma=sigma, dW=dW)
+    esg = ESG(dt_sim=delta_t, dW=dW, B=B, r=r)
+    df_full_run = esg.full_run_to_pandas(dt_out=1, max_t=40)
+    ax=None
+    initial_r = pd.DataFrame.from_records([(0, i, B(0)) for i in range(n_sims)], 
+                                           columns=['time', 'sim', 'r']).set_index(['time', 'sim'])
+    df = pd.concat([initial_r, df_full_run[['r']]])
+    for sim, r in df.groupby(level='sim'):
+        if ax is None:
+            ax = r.reset_index('sim')['r'].plot()
+        else:
+            ax = r.reset_index('sim')['r'].plot(ax=ax)
         
-    for r in sims.values.squeeze():
-        row =  np.insert(r,0,r_zero)
-        x = np.insert(sims.coords['time'].values,0,0)
-        plt.plot(x, row)
+
         
 def plot_short_rate_pcentiles():
-    now_ = datetime.datetime.now()
-    sims_nb=10000
-    over_samp = 52
-    r_gen = rng.NormalRng(shape={'svar':1, 'sim':sims_nb, 'timestep':40*over_samp},
-                            mean=[0], cov=[[1]])
-    HW = ir_models.HullWhite1fModel
-    fixed_b = 0.05
-    b = lambda x: fixed_b
-    r_zero = 0.05
-    hw = HW(a=1.01, b=b, sigma=0.02, r_zero=r_zero, 
-                  delta_t_in=1, delta_t_out=10)
+
+    delta_t = 1/52
+    n_sims= 10_000
+    dW = rng.NormalRng(dims=1, sims=n_sims, mean=[0], cov=[[delta_t]])
+    a = 0.001
+    sigma = 0.01
+    B = TimeDependentParameter(function=lambda t: 0.01)
+    r = HullWhite1fShortRate(B=B, a=a, sigma=sigma, dW=dW)
+    esg = ESG(dt_sim=delta_t, dW=dW, B=B, r=r)
+    df_full_run = esg.full_run_to_pandas(dt_out=1, max_t=40)
+    initial_r = pd.DataFrame.from_records([(0, i, B(0)) for i in range(n_sims)], 
+                                           columns=['time', 'sim', 'r']).set_index(['time', 'sim'])
     
+    sims = pd.concat([initial_r, df_full_run[['r']]]).unstack('time').values
+   
     
-    X = r_gen.generate()
-    sims = hw.transform(X)
-    print(datetime.datetime.now()-now_)
-    x = np.insert(sims.coords['timestep'].values,0,0)
-    sims = np.concatenate([np.ones((1,sims_nb,1))*r_zero, sims], axis=2).squeeze()
-    pctiles = [np.percentile(sims.squeeze(), p, axis=0) for p in [0,25,75,100]]
-    mean = np.median(sims.squeeze(), axis=0)
-    plt.plot(x, mean, 'k')
+    x = np.arange(41)
+    pctiles = [np.percentile(sims, p, axis=0) for p in [0,25,75,100]]
+    median = np.mean(sims, axis=0)
+    print(median)
+    plt.plot(x, median, 'k')
     n = len(pctiles)
     color = plt.cm.Blues(np.linspace(0.4,0.6,n))
 
@@ -71,42 +77,42 @@ def plot_short_rate_pcentiles():
         plt.fill_between(x, p1, p2, facecolor=color[-i], alpha=0.5)
     plt.show()
 
+    
 def plot_short_rate_dist():
     now_ = datetime.datetime.now()
-    sims_nb=10_000
-    time_sampling_factor = 20
-    max_time = 1
-    r_gen = rng.NormalRng(shape={'svar':1, 'sim':sims_nb, 'timestep':max_time*time_sampling_factor},
-                            mean=[0], cov=[[1/time_sampling_factor]])
-    HW = ir_models.HullWhite1fModel
-    fixed_b = 0.05
-    b = lambda x: fixed_b
-    r_zero = 0.05
-    sigma = 0.02
-    a = 1.01
-    hw = HW(a=a, b=b, sigma=sigma, r_zero=r_zero, 
-                  delta_t_in=1/time_sampling_factor, delta_t_out=1)
-    X = r_gen.generate()
-    sims = hw.transform(X)
+    delta_t = 1/5000
+    sim_nb = 10000
+    dW = rng.NormalRng(dims=1, sims=sim_nb, mean=[0], cov=[[delta_t]])
+    T=1
+    a = 0.5
+    mean_rev_level = 0.05
+    sigma = 0.01
+
+    B_fc = lambda t: mean_rev_level + 0.1*t
+    B = TimeDependentParameter(function= B_fc)
+    b = lambda t: 0.1 #derivative of B_fc
+    r = HullWhite1fShortRate(B=B, a=a, sigma=sigma, dW=dW)
+    esg = ESG(dt_sim=delta_t, dW=dW, B=B, r=r)
+    df_full_run = esg.full_run_to_pandas(dt_out=1, max_t=T)['r']
     print(datetime.datetime.now()-now_)
     test_time = 1
-    sims_test = sims.loc[{'timestep':test_time*time_sampling_factor}]
-    theoretical_mu = np.exp(-a*test_time)*r_zero+b(test_time)/a*(1-np.exp(-a*test_time))
-    theoretical_sigma = np.sqrt(sigma**2/2/a*(1-np.exp(-2*a*test_time)))
+    sims_test = df_full_run.values
+    theoretical_mu = B_fc(T)
+    theoretical_sigma = np.sqrt(sigma**2/2/a*(1-np.exp(-2*a*T)))
     n, bins, patches = plt.hist(sims_test, bins=100,  normed=1)
     y = mlab.normpdf(bins, theoretical_mu, theoretical_sigma)
     plt.plot(bins, y, '--')    
     plt.show()
-    print('mean', theoretical_mu, sims_test.values.squeeze().mean())
-    print('sigma', theoretical_sigma, sims_test.values.squeeze().std())
+    print('mean', theoretical_mu, sims_test.squeeze().mean())
+    print('sigma', theoretical_sigma, sims_test.squeeze().std())
 
 
-    _, p1 = normaltest(sims_test.values.squeeze())
+    _, p1 = normaltest(sims_test.squeeze())
 #    z = (sims_test.values.squeeze()-theoretical_mu)/theoretical_sigma
 #    _, p3= kstest(z, 'norm',args=(0,1))
     
     print('p-value normal test (>0.05 is good)', p1)
-    probplot(sims_test.values.squeeze(),
+    probplot(sims_test.squeeze(),
              sparams=(theoretical_mu,theoretical_sigma),fit=False, plot=plt)
     plt.show()
     
@@ -192,4 +198,4 @@ def extract_esg_output():
     df_out.unstack(level='var').dropna(axis=1, how='all').to_clipboard()     
     print("done!")
     
-extract_esg_output()
+plot_short_rate_dist()
