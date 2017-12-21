6887391fa833cb36dafba880a80aef4047b19307
luk-f-a <lucioarjona@hotmail.com>
2017-12-21 18:08:37 +0000

    added tests for independent and dependent normal random variables

--- tests/test_rng.py
+++ tests/test_rng.py
@@ -40,11 +40,41 @@ class Wiener_test(unittest.TestCase):
         self.esg = ESG(dt_sim=delta_t, dW=dW, W=W)
             
     def test_returns_object(self):
-        df_full_run = self.esg.full_run_to_pandas(dt_out=1, max_t=40)
+        df_full_run = self.esg.run_multistep_to_pandas(dt_out=1, max_t=40)
         self.assertEqual(type(df_full_run), pd.DataFrame,
                          'incorrect type')
-        self.assertEqual(df_full_run.shape, (10*40, 2))
+        self.assertEqual(df_full_run.shape, (10, 2*40))
          
+class Normal_Rng_test(unittest.TestCase):
+    """Test uniform rng with numpy output
+    """
+    def setUp(self):
+        self.rng = rng.NormalRng(dims=2, sims=50000, mean=[0,0], cov=np.eye(2,2))
+        cov = np.array([[1,0.7],[0.7,1]])
+        self.corr_norm =  rng.CorrelatedRV(rng=self.rng, input_cov=np.eye(2,2),
+                                           target_cov=cov)
+        self.esg = ESG(dt_sim=1, ind_dW=self.rng, dW=self.corr_norm)
+        
+        
+    def test_independece(self):
+        cf = self.esg.run_multistep_to_pandas(dt_out=1, max_t=5, 
+                                  out_vars=['ind_dW'])
+        ind_dW_0 = xr.DataArray(cf).unstack('dim_1').sel(model=['ind_dW_0'])
+        #print('ind', np.round(np.corrcoef(ind_dW_0.values.squeeze(), rowvar=False),1))        
+        self.assertTrue(np.allclose(np.round(np.corrcoef(ind_dW_0.values.squeeze(), rowvar=False),1), np.eye(5,5)))
+        
+    def test_covariance(self):
+        cf = self.esg.run_multistep_to_pandas(dt_out=1, max_t=5, 
+                                  out_vars=['dW'])
+        dW_0 = xr.DataArray(cf).unstack('dim_1')
+        dW_0= dW_0.stack(svar_time=('model', 'time'))
+        corr = np.round(np.corrcoef(dW_0.values.squeeze(), rowvar=False),1)
+        self.assertTrue(np.allclose(np.diag(corr), np.ones(10)))
+        self.assertTrue(np.allclose(np.diag(corr,5), 0.7*np.ones(5)))
+        self.assertTrue(np.allclose(np.diag(corr,-5), 0.7*np.ones(5)))
+        
+
+        
 if __name__ == '__main__':
     unittest.main()              
             
