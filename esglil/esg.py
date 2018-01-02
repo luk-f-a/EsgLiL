25be1a73e22e05747bb223847780ce581637b81e
luk-f-a <lucioarjona@hotmail.com>
2018-01-02 14:48:04 +0100

    allowing time filter in multistep to pandas

--- esglil/esg.py
+++ esglil/esg.py
@@ -117,19 +117,20 @@ class Model(object):
         return out
     
         
-    def run_multistep_to_pandas_old(self, dt_out, max_t, out_vars=None):
-        import pandas as pd
-        dt_out = float_to_fraction(dt_out)
-        assert max_t > float(self.clock)
-        if dt_out < self.dt_sim:
-            assert self.dt_sim % dt_out == 0
-            return self._dense_run_to_pandas(dt_out, max_t, out_vars)
-        else:
-            assert dt_out % self.dt_sim == 0
-            return self._sparse_run_to_pandas(dt_out, max_t, out_vars)
+#    def run_multistep_to_pandas_old(self, dt_out, max_t, out_vars=None):
+#        import pandas as pd
+#        dt_out = float_to_fraction(dt_out)
+#        assert max_t > float(self.clock)
+#        if dt_out < self.dt_sim:
+#            assert self.dt_sim % dt_out == 0
+#            return self._dense_run_to_pandas(dt_out, max_t, out_vars)
+#        else:
+#            assert dt_out % self.dt_sim == 0
+#            return self._sparse_run_to_pandas(dt_out, max_t, out_vars)
 
     def run_multistep_to_pandas(self, dt_out, max_t, out_vars=None):
-        import pandas as pd
+        if isinstance(out_vars, list):
+            out_vars = {var:slice(None) for var in out_vars}
         dt_out = float_to_fraction(dt_out)
         assert max_t > float(self.clock)
         out = []
@@ -146,56 +147,65 @@ class Model(object):
                     self.eq[model].run_step(float(self.clock))
             if dt_out <= self.dt_sim or ts % float(dt_out/self.dt_sim) == 0:        
                 out.append(self.df_value_t(out_vars))
-                
-
-        if out==[]:
-            print('problem')
-        else:
-            df = pd.concat(out, axis=1)
+        df = pd.concat(out, axis=1)
         return df
         
-    def _sparse_run_to_pandas(self, dt_out, max_t, out_vars=None):
-        """This method makes a run blah blah
-        """
-        out = []
-        nb_steps = int(round(float((max_t-self.clock)/self.dt_sim),0))
-        for ts in range(1, nb_steps+1):
-            self.run_step()
-            if ts % float(dt_out/self.dt_sim) == 0:
-                out.append(self.df_value_t(out_vars))
-
-        df = pd.concat(out, axis=1)
-        return df
-    
-    def _dense_run_to_pandas(self, dt_out, max_t, out_vars=None):
-        """
-        This method allows for a dt_out which is smaller than the dt of the 
-        model, thus allowing to record the full history of sub-models running on
-        a more granular timegrid
-        """
-        out = []
-        nb_steps = int(round(max_t/dt_out,0))
-
-        for ts in range(1, nb_steps+1):
-            t = ts*dt_out
-
-            if t % self.dt_sim == 0:
-                self.run_step()
-                out.append(self.df_value_t(out_vars))
-            else:
-                for model in self.eq:
-                    if hasattr(self.eq[model], 'run_multistep_to_pandas'):
-                        mstep_fc = self.eq[model].run_multistep_to_pandas
-                        m_df = mstep_fc(dt_out, max_t=t, out_vars=out_vars)
-#                        m_df = m_df.rename(index={orig:model+'_'+orig 
-#                                             for orig in m_df.index.get_level_values('model')})
-                        out.append(m_df)
-                        
-        df = pd.concat(out, axis=1)
-        return df
+#    def _sparse_run_to_pandas(self, dt_out, max_t, out_vars=None):
+#        """This method makes a run blah blah
+#        """
+#        out = []
+#        nb_steps = int(round(float((max_t-self.clock)/self.dt_sim),0))
+#        for ts in range(1, nb_steps+1):
+#            self.run_step()
+#            if ts % float(dt_out/self.dt_sim) == 0:
+#                out.append(self.df_value_t(out_vars))
+#
+#        df = pd.concat(out, axis=1)
+#        return df
+#    
+#    def _dense_run_to_pandas(self, dt_out, max_t, out_vars=None):
+#        """
+#        This method allows for a dt_out which is smaller than the dt of the 
+#        model, thus allowing to record the full history of sub-models running on
+#        a more granular timegrid
+#        """
+#        out = []
+#        nb_steps = int(round(max_t/dt_out,0))
+#
+#        for ts in range(1, nb_steps+1):
+#            t = ts*dt_out
+#
+#            if t % self.dt_sim == 0:
+#                self.run_step()
+#                out.append(self.df_value_t(out_vars))
+#            else:
+#                for model in self.eq:
+#                    if hasattr(self.eq[model], 'run_multistep_to_pandas'):
+#                        mstep_fc = self.eq[model].run_multistep_to_pandas
+#                        m_df = mstep_fc(dt_out, max_t=t, out_vars=out_vars)
+##                        m_df = m_df.rename(index={orig:model+'_'+orig 
+##                                             for orig in m_df.index.get_level_values('model')})
+#                        out.append(m_df)
+#                        
+#        df = pd.concat(out, axis=1)
+#        return df
 
     def df_value_t(self, out_vars):
         import pandas as pd
+        def get_this_model_step(name):
+            if (out_vars is None):
+                return True
+            if not name in out_vars:
+                return False
+            else:
+                if (((out_vars[name].start is None) or 
+                    out_vars[name].start <= self.clock) and
+                    ((out_vars[name].stop is None) or 
+                out_vars[name].stop >= self.clock)):
+                        return True
+                else:
+                    return False
+                
         value_dict = {}
         df_list = []
         for model in self.eq:
@@ -205,26 +215,15 @@ class Model(object):
                 model_val = self.eq[model].value_t
                 if isinstance(model_val, dict):
                     for sub_output in model_val:
-                        if (out_vars is None) or (sub_output in out_vars):
+                        if get_this_model_step(sub_output):
                             val = model_val[sub_output]
                             #key_name = model+'_'+sub_output
                             key_name = sub_output
                             v_d = value_to_dictionary(key_name, val)
                             value_dict.update(v_d)
                 else:
-                    if (out_vars is None) or (model in out_vars):
+                    if get_this_model_step(model):
                         value_dict.update(value_to_dictionary(model, model_val))
-#        df = pd.DataFrame.from_dict(value_dict).assign(time=self.clock)
-#        df.index.names = ['sim']
-#        df.columns.names = ['model']
-#        df = df.set_index(['time'], append=True)
-#        df = df.unstack('time')
-#        df = pd.concat([df]+df_list, axis=1)
-#        
-        #df = df.stack()
-#        if isinstance(df, pd.Series):
-#            df = df.to_frame()
-
         df = pd.DataFrame.from_dict(value_dict)
         tuples = list(zip(df.columns, repeat(self.clock)))
         index = pd.MultiIndex.from_tuples(tuples, names=['model', 'time'])
@@ -233,45 +232,7 @@ class Model(object):
         return df
         
     
-    def df_value_t_old(self, out_vars):
-        import pandas as pd
-        value_dict = {}
-        df_list = []
-        for model in self.eq:
-            if hasattr(model, 'df_value_t'):
-                val_df = model.df_value_t(out_vars)
-                df_list.append(val_df)
-                #maybe add here model name as prefix to the columns names 
-            else:
-                model_val = self.eq[model].value_t
-                if isinstance(model_val, dict):
-                    for sub_output in model_val:
-                        if (out_vars is None) or (sub_output in out_vars):
-                            val = model_val[sub_output]
-                            #key_name = model+'_'+sub_output
-                            key_name = sub_output
-                            v_d = value_to_dictionary(key_name, val)
-                            value_dict.update(v_d)
-                else:
-                    if (out_vars is None) or (model in out_vars):
-                        value_dict.update(value_to_dictionary(model, model_val))
-#        df = pd.DataFrame.from_dict(value_dict).assign(time=self.clock)
-#        df.index.names = ['sim']
-#        df.columns.names = ['model']
-#        df = df.set_index(['time'], append=True)
-#        df = df.unstack('time')
-#        df = pd.concat([df]+df_list, axis=1)
-#        
-        #df = df.stack()
-#        if isinstance(df, pd.Series):
-#            df = df.to_frame()
 
-        df = pd.DataFrame.from_dict(value_dict)
-        tuples = list(zip(df.columns, repeat(self.clock)))
-        index = pd.MultiIndex.from_tuples(tuples, names=['model', 'time'])
-        df.columns = index
-        df.index.names = ['sim']
-        return df
         
     
 def value_to_dictionary(model_name, val):
