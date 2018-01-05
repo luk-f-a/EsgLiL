89d3191261b7e8093a17c731d7fe1712bac6859d
luk-f-a <lucioarjona@hotmail.com>
2018-01-05 16:40:33 +0000

    Update model_zoo.py

--- esglil/model_zoo.py
+++ esglil/model_zoo.py
@@ -15,9 +15,11 @@ from esglil.esg import ESG
 from esglil.equity_models import GeometricBrownianMotion, GBM_exact
 from esglil import rng
 
-def esg_e_sr_bonds_cash(delta_t, sims, rho, bond_prices, hw_a = 0.001, hw_sigma = 0.01,
-                           gbm_sigma=0.2):
-
+def esg_e_sr_bonds_cash(delta_t, sims, rho, bond_prices, hw_a = 0.001, 
+                        hw_sigma = 0.01, gbm_sigma=0.2):
+    """Returns and esg model with equity, bonds and cash, driven by a gbm
+    and a hw-1f model respectively
+    """
     
     assert type(bond_prices) is dict
 #    assert len(bond_prices.shape)==2
@@ -78,40 +80,111 @@ def esg_e_sr_bonds_cash_2levels(delta_t_l1, delta_t_l2, sims, rho, bond_prices, 
     
     return esg_l2
 
-def esg_e_sr_bonds_cash_test(delta_t, sims, rho, bond_prices, hw_a = 0.001, hw_sigma = 0.01,
-                           gbm_sigma=0.2):
-    assert type(bond_prices) is dict
-#    assert len(bond_prices.shape)==2
-#    assert bond_prices.shape[1]==1
+def get_gbm_hw_nobonds(delta_t, sims, rho, bond_prices, 
+                       hw_a = 0.001, hw_sigma = 0.01, gbm_sigma=0.2, 
+                       custom_ind_dw=None,
+                       use_dask=False, use_cores=1):
+    """Returns an esg model with short rate, cash and equity  
+    using a GBM and HW models.
+    
+    delta_t: float or Fraction
+        time delta to use for euler simulation
+      
+    sims: int
+        amount of simulations to run
+    
+    rho: float 
+        correlation between HW and GBM Brownian motions.
+    
+    bond_prices: dictionary {maturity: price}
+        prices for notional 1 zero-coupon bonds, ie discount factors.
+        
+    use_dask: whether to use dask instead of numpy
+    
+    use_cores: if using dask, how many cores (chunks) should be used.
+    """
+    if custom_ind_dw:
+        ind_dW = custom_ind_dw
+    else:
+        ind_dW = rng.IndWienerIncr(dims=2, sims=sims, mean=0, delta_t=delta_t,
+                                   use_dask=use_dask, use_cores=use_cores)
     corr = [[1, rho], [rho, 1]]
     C = np.diag([np.sqrt(delta_t), np.sqrt(delta_t)])
     dep_cov = C@corr@C.T 
     indep_cov = np.diag([delta_t, delta_t])
-    ind_dW = rng.NormalRng(dims=2, sims=sims, mean=[0, 0], cov=indep_cov)
     dW = rng.CorrelatedRV(rng=ind_dW, input_cov=indep_cov , target_cov=dep_cov)
-    B_fc, f, p0 = hw1f_B_function(bond_prices=bond_prices, a=hw_a, sigma=hw_sigma,
-                           return_p_and_f=True)
+    B_fc = hw1f_B_function(bond_prices=bond_prices, a=hw_a, sigma=hw_sigma,
+                           return_p_and_f=False)
     B = TimeDependentParameter(B_fc)
     r = HullWhite1fShortRate(B=B, a=hw_a, sigma=hw_sigma, dW=dW[0])
-    
-    T = np.array(list(bond_prices.keys())).reshape(-1,1)
-    P = HullWhite1fBondPrice(a=hw_a, r=r, sigma=hw_sigma,  
-                             P_0=p0, f=f, T=T)
     C = HullWhite1fCashAccount(r=r)
     W = rng.WienerProcess(dW)
-    #S = GeometricBrownianMotion(mu=r, sigma=gbm_sigma, dW=dW[1])
-    S = GeometricBrownianMotion(mu=0, sigma=gbm_sigma, dW=ind_dW[1])
-    esg = ESG(dt_sim=delta_t, ind_dW=ind_dW, dW=dW, W=W, B=B, r=r, cash=C, P=P, S=S)
-    
+    ind_W = rng.WienerProcess(ind_dW)
+    S = GeometricBrownianMotion(mu=r, sigma=gbm_sigma, dW=dW[1])
+    esg = ESG(dt_sim=delta_t, ind_dW=ind_dW, dW=dW, W=W, ind_W=ind_W,
+              B=B, r=r, cash=C,  S=S)
     return esg
+    
+def get_gbm_hw_2levels(delta_t_l1, delta_t_l2, sims, rho, bond_prices, 
+                       hw_a = 0.001, hw_sigma = 0.01,
+                       gbm_sigma=0.2, const_tau=None, out_bonds=None,
+                       use_dask=False, use_cores=1):
+    """Returns an esg model loop with short_rate cash, equity on a more granular
+    simulation level and a second level loop with a different delta_t for
+    bonds and/or constant maturity bonds using a GBM and HW models.
+    The first level is built using get_gbm_hw_nobonds
+    
+    delta_t_l1: float or Fraction
+        time delta to use for models that require euler simulation. This
+        parameter is passed to get_gbm_hw_nobonds
+    
+    delta_t_l2: float or Fraction
+        time delta to use for HW bonds (which use a closed form price)
+        
+    sims, rho, bond_prices: see get_gbm_hw_nobonds documentation.
+        
+    const_tau: 1-d array
+        constant time to maturity to track bond prices (instead of calculating
+        all prices at every step)
+        
+    out_bonds: 1-d array
+        maturity of bonds to track at every time step
+        
+    use_dask: whether to use dask instead of numpy
+    
+    use_cores: if using dask, how many cores (chunks) should be used.
+    """
+    assert type(bond_prices) is dict
+    B_fc, f, p0 = hw1f_B_function(bond_prices=bond_prices, a=hw_a, sigma=hw_sigma,
+                           return_p_and_f=True)
+    esg_l1 = get_gbm_hw_nobonds(delta_t_l1, sims, rho, bond_prices, 
+                       hw_a, hw_sigma, gbm_sigma, None, use_dask, use_cores)
+    opt_kwargs = {}
+    if const_tau is not None:
+        hw_const_bond = HullWhite1fConstantMaturityBondPrice
+        constP = hw_const_bond(a=hw_a, r=esg_l1['r'], sigma=hw_sigma,  
+                               P_0=p0, f=f, tau=const_tau.reshape(-1,1))
+        opt_kwargs['constP'] = constP
+        
+    if out_bonds is not None:
+        T = out_bonds
+        P = HullWhite1fBondPrice(a=hw_a, r=esg_l1['r'], sigma=hw_sigma, 
+                                 P_0=p0, f=f, T=T)
+        opt_kwargs['P'] = P
+ 
+    esg_l2 = ESG(dt_sim=delta_t_l2, esg_l1=esg_l1, **opt_kwargs)
+    return esg_l2
 
-def esg_equity(delta_t, sims, r=0.03, gbm_sigma=0.2):
-    dW = rng.NormalRng(dims=1, sims=sims, mean=[0], cov=[[float(delta_t)]])
+def esg_equity(delta_t, sims, r=0.03, gbm_sigma=0.2, use_dask=False):
+    dW = rng.IndWienerIncr(dims=1, sims=sims, mean=0, delta_t=delta_t,
+                                   use_dask=use_dask, use_cores=1)
     W = rng.WienerProcess(dW)
     S = GeometricBrownianMotion(mu=r, sigma=gbm_sigma, dW=dW)
     esg = ESG(dt_sim=delta_t, dW=dW, W=W, S=S)
     
     return esg
+
+
 
 def esg_equity2(delta_t, sims, r=0.03, gbm_sigma=0.2):
     """covar calculations
