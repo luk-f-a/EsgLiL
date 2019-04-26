76de601d3b0216959da6bfa9d56239131581b83d
luk-f-a <lucioarjona@hotmail.com>
2019-04-26 18:57:13 +0200

    added european option pricing model

---
+++ esglil/pricing_models/black_scholes.py
@@ -0,0 +1,97 @@
+import numpy as np
+from esglil.common import StochasticVariable
+
+try:
+    import QuantLib as ql
+except ModuleNotFoundError:
+    pass
+except:
+    raise
+
+class BlackScholeEuropeanPrice(StochasticVariable):
+    """class for pricing European Swaptions (cash settled) using a
+    1F HW model with Jamshidian decomposition
+
+     Parameters
+    ----------
+
+    maturity: integer
+        maturity (number of years between beginning of simulation and payment)
+
+    tenor: integer
+        tenor or underlying swap in years
+
+    bonds_dict:  dictionary or mapping object
+        dictionary of BondPrice objects. These objects need to be registered
+        with the Model, so that their price is updated.
+        The dictionary must have entries for every integer between
+        maturity and tenor + 1, both endpoints included.
+    """
+
+    __slots__ = ['mat_bond', 'maturity', 'rate', 'ql_option', 'ql_date',
+                 'calendar', 'underlying', 'underlyingQuote' ]
+
+    def __init__(self, maturity, strike, flat_vol, opt_type,
+                 underlying, maturity_bond):
+        assert opt_type in ('call', 'put')
+        self.maturity = maturity
+        self.underlying = underlying
+        ql_opt_type = ql.Option.Call if opt_type == 'call' else ql.Option.Put
+        calendar = ql.TARGET()
+        self.calendar = calendar
+
+        #the today date is irrelevant but QuantLib needs to build a calendar
+        today = ql.Date(2, ql.January, 2019)
+        self.ql_date = today
+        maturity_date = calendar.advance(today, maturity, ql.Years)
+        ql.Settings.instance().evaluationDate = today
+        # as long as always working with annual rates, the day count should be irrelevant
+        day_count = ql.Actual365Fixed()
+        self.mat_bond = maturity_bond
+        ir_rate = ql.SimpleQuote(maturity_bond ** (-1 / maturity) - 1)
+        self.rate = ir_rate
+        rateHandle = ql.QuoteHandle(ir_rate)
+        riskFreeRate = ql.FlatForward(today, rateHandle, day_count)
+        discount_curve = ql.YieldTermStructureHandle(riskFreeRate)
+        notional = 1e6
+
+        # surface
+        volatility = ql.BlackConstantVol(0, calendar, flat_vol, day_count)
+
+        # option parameters
+        exercise = ql.EuropeanExercise(maturity_date)
+        payoff = ql.PlainVanillaPayoff(ql_opt_type, strike)
+
+        # market data
+        underlyingQuote = ql.SimpleQuote(underlying.value_t)
+        self.underlyingQuote = underlyingQuote
+        divRate = 0
+        dividendYield = ql.FlatForward(today, divRate, day_count)
+        process = ql.BlackScholesMertonProcess(ql.QuoteHandle(underlyingQuote),
+                                               ql.YieldTermStructureHandle(dividendYield),
+                                               discount_curve,
+                                               ql.BlackVolTermStructureHandle(volatility))
+
+        option = ql.EuropeanOption(payoff, exercise)
+        engine = ql.AnalyticEuropeanEngine(process)
+
+        # method: analytic
+        option.setPricingEngine(engine)
+        self.ql_option = option
+
+        self.value_t = None
+        self.t_1 = 0
+
+    def run_step(self, t):
+        self.ql_date = self.calendar.advance(self.ql_date, int(t - self.t_1),
+                                             ql.Years)
+        ql.Settings.instance().evaluationDate = self.ql_date
+
+        self.value_t = np.zeros(self.mat_bond.value_t.shape[-1])
+        new_rate_val = self.mat_bond ** (-1 / self.maturity) - 1
+        new_underlying_val = self.underlying.value_t
+        for i in range(len(self.value_t)):
+            self.underlyingQuote.setValue(new_underlying_val[i])
+            self.rate.setValue(new_rate_val[i])
+            self.value_t[i] = self.ql_option.NPV()
+        self.t_1 = t
--- tests/test_pricing.py
+++ tests/test_pricing.py
@@ -8,8 +8,11 @@ from esglil.ir_models.hw1f_annual_exact import (ShortRate, CashAccount,
                                                 BondPrice)
 from esglil.ir_models.hw1f_annual_exact import (get_g_function, get_h_function,
                                                 calibrate_b_function, calc_r_zero)
+from esglil.pricing_models.black_scholes import BlackScholeEuropeanPrice
 from esglil.pricing_models.hw1f import HW1fSwaptionPrice
+from esglil.equity_models import GeometricBrownianMotion
 
+import numpy as np
 
 class hw1f_test_swaption_price(unittest.TestCase):
     def setUp(self):
@@ -51,6 +54,52 @@ class hw1f_test_swaption_price(unittest.TestCase):
                          'incorrect type')
         self.assertEqual(len(results), 8)
         self.assertTrue('swaption' in results[1])
+
+
+
+class BlackScholesTestOptionPrice(unittest.TestCase):
+    def setUp(self):
+        #basic swaption information
+        maturity = 10
+        #esg basic elements
+        bond_prices = {t: 1.01**(-t) for t in range(1,41)}
+        delta_t = 1
+        rho = 0.2
+        corr = [[1, rho], [rho, 1]]
+        C = np.diag([delta_t, delta_t])
+        cov = C*corr*C.T
+        dW = rng.NormalRng(dims=2, sims=1000, mean=[0, 0], cov=cov)
+        hw_alpha = 0.001
+        hw_sigma = 0.01
+        b_vec = calibrate_b_function(bond_prices, hw_alpha, hw_sigma)
+        b_fc = lambda t: b_vec[int(t)]
+        g = get_g_function(b_s=b_fc, alpha=hw_alpha)
+        h = get_h_function(b=b_fc, alpha=hw_alpha)
+        r = ShortRate(g=g, sigma_hw=hw_sigma,
+                              alpha_hw=hw_alpha, r_zero=0.02, Z=dW[0])
+        volatility = 0.1
+        S = GeometricBrownianMotion(mu=r, sigma=volatility, dW=dW[1])
+        #bonds
+        P = BondPrice(alpha=hw_alpha, r=r, sigma_hw=hw_sigma,
+                             h=h, T=maturity)
+
+        #call
+        strike = 200
+        volatility = 0.1
+        option = BlackScholeEuropeanPrice(maturity, strike, volatility,
+                                     opt_type='call', underlying=S,
+                                          maturity_bond=P)
+
+        #put together esg
+        self.esg = ESG(dt_sim=delta_t, dW=dW, r=r, bond=P, S=S, option=option)
+
+    def test_shape(self):
+        results = self.esg.run_multistep_to_dict(dt_out=1, max_t=10)
+        print([results[time]['option'].mean() for time in results])
+        self.assertEqual(type(results), dict,
+                         'incorrect type')
+        self.assertEqual(len(results), 10)
+        self.assertTrue('option' in results[1])
 
 
 if __name__ == '__main__':
